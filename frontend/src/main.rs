895074626982b2ddf501239dfcc3f2a4b2c3bcf4
Sachin Iyer <sachinjiyer@gmail.com>
2023-07-19 03:55:10 -0700

    weird compile error with msg

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,47 +1,51 @@
-use std::collections::HashMap;
-
-use gloo::file::callbacks::FileReader;
+use dotenv::dotenv;
+use gloo::file::callbacks::{read_as_bytes, FileReader};
 use gloo::file::File;
+use gloo::timers::callback::Timeout;
+use gloo_net::http::Request;
+use serde_json;
+use serde_json::json;
+use serde_json::Value;
+use std::collections::HashMap;
+use std::env;
+use wasm_bindgen::JsValue;
 use web_sys::{DragEvent, Event, FileList, HtmlInputElement};
 use yew::TargetCast;
 use yew::{html, Callback, Component, Context, Html};
 
 struct FileDetails {
-    name: String,
     url: String,
+    status: bool,
 }
 
 pub enum Msg {
     Loaded(String, String, Vec<u8>),
     Files(Vec<File>),
+    FetchBegin,
+    FetchEnd,
 }
 
 pub struct App {
     readers: HashMap<String, FileReader>,
-    files: Vec<FileDetails>,
+    files: HashMap<String, FileDetails>,
+    timeout: Option<Timeout>,
 }
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
+    fn create(ctx: &Context<Self>) -> Self {
+        ctx.link().send_message(Msg::FetchBegin);
         Self {
             readers: HashMap::default(),
-            files: Vec::default(),
+            files: HashMap::default(),
+            timeout: None,
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::Loaded(file_name, _file_type, _data) => {
-                self.files.push(FileDetails {
-                    name: file_name.clone(),
-                    url: "https://".to_string() + &file_name.clone(),
-                });
-                self.readers.remove(&file_name);
-                true
-            }
             Msg::Files(files) => {
                 for file in files.into_iter() {
                     let file_name = file.name();
@@ -51,7 +55,7 @@ impl Component for App {
                         let link = ctx.link().clone();
                         let file_name = file_name.clone();
 
-                        gloo::file::callbacks::read_as_bytes(&file, move |res| {
+                        read_as_bytes(&file, move |res| {
                             link.send_message(Msg::Loaded(
                                 file_name,
                                 file_type,
@@ -61,6 +65,64 @@ impl Component for App {
                     };
                     self.readers.insert(file_name, task);
                 }
+                true
+            }
+            Msg::Loaded(file_name, file_type, data) => {
+                let url = env::var("API").unwrap() + "/";
+                wasm_bindgen_futures::spawn_local(async move {
+                    let body = json!({
+                        "file_name": file_name.into_bytes(),
+                        "file_type": file_type.into_bytes(),
+                        "file_data": String::from_utf8_lossy(&data).to_string(),
+                    });
+
+                    let js_value = JsValue::from_str(body.to_string().as_str());
+                    let _response = Request::post(url.as_str())
+                        .body(js_value)
+                        .unwrap()
+                        .send()
+                        .await
+                        .unwrap()
+                        .status();
+                });
+                true
+            }
+            Msg::FetchBegin => {
+                let url = env::var("API").unwrap() + "/";
+                let ctx = ctx.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let response: Value = Request::get(url.as_str())
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+                    self.files = response
+                        .as_array()
+                        .unwrap()
+                        .iter()
+                        .map(|k| {
+                            let item = k.as_object().unwrap();
+                            (
+                                item["file_name"].as_str().unwrap().to_string(),
+                                FileDetails {
+                                    url: item["file_url"].as_str().unwrap().to_string(),
+                                    status: item["status"].as_bool().unwrap(),
+                                },
+                            )
+                        })
+                        .collect();
+                    ctx.link().send_message(Msg::FetchEnd);
+                });
+                true
+            }
+            Msg::FetchEnd => {
+                let ctx = ctx.clone();
+                let handle = Timeout::new(15000, || {
+                    ctx.link().send_message(Msg::FetchBegin);
+                });
+                self.timeout = Some(handle);
                 true
             }
         }
@@ -100,7 +162,7 @@ impl Component for App {
                     })}
                 />
                 <div id="preview-area">
-                    { for self.files.iter().map(Self::view_file) }
+                    { for self.files.iter().map(|(k, v)| Self::view_file(k.clone(), v)) }
                 </div>
             </div>
         }
@@ -108,10 +170,11 @@ impl Component for App {
 }
 
 impl App {
-    fn view_file(file: &FileDetails) -> Html {
+    fn view_file(name: String, file: &FileDetails) -> Html {
         html! {
-                <a class="preview-title" href={ file.url.clone() } download={ file.name.clone() }>
-                <button class="preview-button">{ format!("{}", file.name) }</button>
+                <a class="preview-title" href={ file.url.clone() } download={ name.clone() }>
+                    <button class="preview-button">{ format!("{}", name) }</button>
+                    <p> { file.status } </p>
                 </a>
         }
     }
@@ -132,5 +195,7 @@ impl App {
 }
 
 fn main() {
+    dotenv().ok();
+    env::var("API").expect("API not set");
     yew::Renderer::<App>::new().render();
 }
