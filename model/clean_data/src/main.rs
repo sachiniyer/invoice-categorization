7206443462efc60eac93dc4a2595ac3b24efbb57
Sachin Iyer <sachinjiyer@gmail.com>
2023-08-03 19:50:37 -0700

    rename columns and write metadata

--- model/clean_data/src/main.rs
+++ model/clean_data/src/main.rs
@@ -4,6 +4,7 @@ use rust_xlsxwriter::*;
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 use std::fs;
+use std::fs::OpenOptions;
 use std::io::{self, Write};
 
 #[derive(Debug, Clone)]
@@ -20,6 +21,27 @@ impl Display for DataTypeWrap {
             DataType::Int(ref n) => write!(f, "{}", n),
             DataType::Error(ref _e) => write!(f, "{}", "Error"),
             DataType::Bool(ref b) => write!(f, "{}", b),
+        }
+    }
+}
+
+enum YNSEnum {
+    Yes,
+    No,
+    Skip,
+}
+
+#[derive(Debug, Clone)]
+enum OffsetEnum {
+    Offset(usize),
+    Skip,
+}
+
+impl Display for OffsetEnum {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            OffsetEnum::Offset(n) => write!(f, "{}", n),
+            OffsetEnum::Skip => write!(f, "{}", "Skip"),
         }
     }
 }
@@ -71,11 +93,26 @@ fn get_user_input() -> Result<String, Box<dyn std::error::Error>> {
     Ok(input_text.trim().to_string())
 }
 
-fn get_user_num() -> Result<usize, Box<dyn std::error::Error>> {
+fn get_user_yns() -> Result<YNSEnum, Box<dyn std::error::Error>> {
+    match get_user_input().unwrap().as_str() {
+        "y" => Ok(YNSEnum::Yes),
+        "n" => Ok(YNSEnum::No),
+        "s" => Ok(YNSEnum::Skip),
+        _ => Err(Box::new(io::Error::new(
+            io::ErrorKind::Other,
+            "Invalid input",
+        ))),
+    }
+}
+
+fn get_user_num() -> Result<OffsetEnum, Box<dyn std::error::Error>> {
     let input_text = get_user_input()?;
+    if input_text == "s" {
+        return Ok(OffsetEnum::Skip);
+    }
     let number = input_text.trim().parse();
     match number {
-        Ok(number) => Ok(number),
+        Ok(number) => Ok(OffsetEnum::Offset(number)),
         Err(e) => Err(Box::new(e)),
     }
 }
@@ -85,15 +122,15 @@ fn get_sheet(workbook: &mut Excel) -> Result<String, Box<dyn std::error::Error>>
     print!("Choose Sheet: ");
     let index = loop {
         match get_user_num() {
-            Ok(index) => {
+            Ok(OffsetEnum::Offset(index)) => {
                 if index >= workbook.sheet_names().unwrap().len() {
                     println!("Invalid input");
                     continue;
                 }
                 break index;
             }
-            Err(e) => {
-                println!("Invalid input {}", e);
+            _ => {
+                println!("Invalid input");
                 print!("Choose Sheet: ");
                 continue;
             }
@@ -107,15 +144,15 @@ fn get_row_offset(range: &mut Range, message: String) -> Result<usize, Box<dyn s
     print!("{}", message);
     let row = loop {
         match get_user_num() {
-            Ok(index) => {
+            Ok(OffsetEnum::Offset(index)) => {
                 if index >= range.rows().count() {
                     println!("Invalid input");
                     continue;
                 }
                 break index;
             }
-            Err(e) => {
-                println!("Invalid Input: {}", e);
+            _ => {
+                println!("Invalid Input");
                 print!("{}", message);
                 continue;
             }
@@ -127,7 +164,7 @@ fn get_row_offset(range: &mut Range, message: String) -> Result<usize, Box<dyn s
 fn get_column_offset(
     row: Vec<Vec<DataTypeWrap>>,
     message: String,
-) -> Result<usize, Box<dyn std::error::Error>> {
+) -> Result<OffsetEnum, Box<dyn std::error::Error>> {
     let row: Vec<Vec<DataTypeWrap>> = (0..row[0].len())
         .map(|col| row.iter().map(|row| row[col].clone()).collect())
         .collect();
@@ -144,13 +181,18 @@ fn get_column_offset(
     print!("{}", message);
     let column = loop {
         match get_user_num() {
-            Ok(index) => {
-                if index >= row.len() {
-                    println!("Invalid input");
-                    continue;
+            Ok(index) => match index {
+                OffsetEnum::Offset(index) => {
+                    if index >= row.len() {
+                        println!("Invalid input");
+                        continue;
+                    }
+                    break OffsetEnum::Offset(index);
                 }
-                break index;
-            }
+                OffsetEnum::Skip => {
+                    break OffsetEnum::Skip;
+                }
+            },
             Err(e) => {
                 println!("{}", e);
                 print!("{}", message);
@@ -163,7 +205,7 @@ fn get_column_offset(
 
 fn get_offset(
     range: &mut Range,
-) -> Result<(usize, HashMap<String, usize>), Box<dyn std::error::Error>> {
+) -> Result<(usize, HashMap<String, OffsetEnum>), Box<dyn std::error::Error>> {
     let row = get_row_offset(range, "Choose Row: ".to_string())?;
     let row_iter = range.rows();
     let row_iter = row_iter.skip(row);
@@ -189,16 +231,23 @@ fn get_offset(
 fn get_column(
     range: &mut Range,
     row_offset: usize,
-    column: usize,
+    column: OffsetEnum,
 ) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let mut res = vec![];
     for i in row_offset..range.get_size().0 {
-        res.push(
-            DataTypeWrap {
-                data_type: range.get_value(i, column).clone(),
+        match column {
+            OffsetEnum::Offset(index) => {
+                res.push(
+                    DataTypeWrap {
+                        data_type: range.get_value(i, index).clone(),
+                    }
+                    .to_string(),
+                );
             }
-            .to_string(),
-        );
+            OffsetEnum::Skip => {
+                res.push("".to_string());
+            }
+        }
     }
     Ok(res)
 }
@@ -207,7 +256,13 @@ fn write_column(
     worksheet: &mut Worksheet,
     data: Vec<String>,
     column: usize,
+    name: Option<String>,
 ) -> Result<&mut Worksheet, Box<dyn std::error::Error>> {
+    let mut data = data.clone();
+    data[0] = match name {
+        Some(name) => name.to_string(),
+        None => data[0].clone(),
+    };
     Ok(worksheet.write_column(0, column as u16, data)?)
 }
 
@@ -215,55 +270,93 @@ fn write_file(
     range: &mut Range,
     row_offset: usize,
     split: usize,
-    columns: HashMap<String, usize>,
+    columns: HashMap<String, OffsetEnum>,
 ) -> Result<Workbook, Box<dyn std::error::Error>> {
     let mut workbook = Workbook::new();
     let worksheet = workbook.add_worksheet().set_name("Data")?;
     let mut iter = 0;
-    for (_, v) in columns.into_iter() {
+    for (i, v) in columns.into_iter() {
         let data = get_column(range, row_offset, v).unwrap();
-        let _ = write_column(worksheet, data, iter);
+        let _ = write_column(worksheet, data, iter, Some(i));
         iter += 1;
     }
     for i in split..range.get_size().1 {
-        let data = get_column(range, row_offset, i).unwrap();
-        let _ = write_column(worksheet, data, iter);
+        let data = get_column(range, row_offset, OffsetEnum::Offset(i)).unwrap();
+        let _ = write_column(worksheet, data, iter, None);
         iter += 1;
     }
     Ok(workbook)
 }
 
-fn confirm_save() -> Result<bool, Box<dyn std::error::Error>> {
-    print!("Save? (y/n): ");
-    let res = loop {
-        match get_user_input() {
-            Ok(s) => {
-                if s == "y" {
-                    break true;
-                } else if s == "n" {
-                    break false;
-                } else {
-                    println!("Invalid input");
-                    print!("Save? (y/n): ");
-                    continue;
-                }
-            }
-            Err(e) => {
-                println!("Invalid input: {}", e);
-                print!("Save? (y/n): ");
+fn confirm_save() -> Result<YNSEnum, Box<dyn std::error::Error>> {
+    print!("Save? (y/n/s): ");
+    loop {
+        match get_user_yns() {
+            Ok(YNSEnum::Yes) => return Ok(YNSEnum::Yes),
+            Ok(YNSEnum::No) => return Ok(YNSEnum::No),
+            Ok(YNSEnum::Skip) => return Ok(YNSEnum::Skip),
+            _ => {
+                println!("Invalid input");
+                print!("Save? (y/n/s): ");
                 continue;
             }
         }
-    };
-    Ok(res)
+    }
 }
 
-fn process_file(file_path: &std::path::Path, output_dir: &str) -> io::Result<()> {
+fn use_saved(file_name: &str, output_dir: &str) -> Result<bool, Box<dyn std::error::Error>> {
+    let out_file_name = format!("{}{}", output_dir, file_name);
+    if std::path::Path::new(&out_file_name).exists() {
+        println!("{} already exists", out_file_name);
+        print!("Overwrite? (y/n/s): ");
+        loop {
+            match get_user_yns() {
+                Ok(YNSEnum::Yes) => return Ok(true),
+                Ok(YNSEnum::No) => return Ok(false),
+                Ok(YNSEnum::Skip) => return Ok(true),
+                _ => {
+                    println!("Invalid input");
+                    print!("Overwrite? (y/n/s): ");
+                    continue;
+                }
+            }
+        }
+    }
+    Ok(false)
+}
+
+fn write_metadata(
+    file_name: &str,
+    row_offset: usize,
+    columns: HashMap<String, OffsetEnum>,
+    meta_file: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut meta_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(meta_file)?;
+    let mut metadata = HashMap::new();
+    metadata.insert("file_name".to_string(), file_name.to_string());
+    metadata.insert("row_offset".to_string(), row_offset.to_string());
+    for (k, v) in columns.into_iter() {
+        metadata.insert(k, v.to_string());
+    }
+    let metadata = serde_json::to_string(&metadata)?;
+    meta_file.write_all(metadata.as_bytes())?;
+    Ok(())
+}
+
+fn process_file(file_path: &std::path::Path, output_dir: &str, meta_file: &str) -> io::Result<()> {
     let file_name = file_path
         .file_name()
         .unwrap()
         .to_str()
         .expect("Invalid filename");
+
+    if use_saved(file_name, output_dir).unwrap() {
+        return Ok(());
+    }
+
     println!("File: {}", file_name);
     let mut workbook = Excel::open(file_path).unwrap();
     let sheet = get_sheet(&mut workbook).unwrap();
@@ -275,29 +368,40 @@ fn process_file(file_path: &std::path::Path, output_dir: &str) -> io::Result<()>
     println!("Column offset: {:?}", column_offsets);
 
     let split = column_offsets.remove("split").unwrap();
+    let split = match split {
+        OffsetEnum::Offset(x) => x,
+        _ => panic!("Invalid split column"),
+    };
     let mut new_workbook = write_file(
         &mut workbook.worksheet_range(&sheet).unwrap(),
         row_offset,
         split,
-        column_offsets,
+        column_offsets.clone(),
     )
     .unwrap();
     let output_file = format!("{}/{}", output_dir, file_name);
-    if confirm_save().unwrap() {
-        new_workbook.save(output_file).unwrap();
-        return Ok(());
+    match confirm_save().unwrap() {
+        YNSEnum::Yes => {
+            new_workbook.save(&output_file).unwrap();
+            write_metadata(file_name, row_offset, column_offsets, meta_file).unwrap();
+            Ok(())
+        }
+        YNSEnum::No => Err(io::Error::new(
+            io::ErrorKind::Other,
+            "File not saved".to_string(),
+        ))?,
+        YNSEnum::Skip => return Ok(()),
     }
-    Err(io::Error::new(io::ErrorKind::Other, "User cancelled"))
 }
 
-fn process_files(input_dir: &str, output_dir: &str) -> io::Result<()> {
+fn process_files(input_dir: &str, output_dir: &str, meta_file: &str) -> io::Result<()> {
     let files = fs::read_dir(input_dir)?;
 
     for file in files {
         let file_path = file?.path();
         if file_path.is_file() {
             loop {
-                match process_file(&file_path, output_dir) {
+                match process_file(&file_path, output_dir, meta_file) {
                     Ok(_) => break,
                     Err(e) => {
                         println!("Error: {}", e);
@@ -328,12 +432,19 @@ fn main() {
                 .long("output")
                 .value_name("OUTPUT"),
         )
+        .arg(
+            Arg::new("metadata")
+                .short('m')
+                .long("metadata")
+                .value_name("METADATA"),
+        )
         .get_matches();
 
     let input = matches.get_one::<String>("input").unwrap();
     let output = matches.get_one::<String>("output").unwrap();
+    let metadata = matches.get_one::<String>("metadata").unwrap();
 
-    match process_files(input, output) {
+    match process_files(input, output, metadata) {
         Ok(_) => println!("All files processed successfully!"),
         Err(err) => eprintln!("Error: {}", err),
     }
